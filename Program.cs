5653ae15a83ab7d1d47006852f4d790317555aca
fedorrb <elposhta@bigmir.net>
2017-07-27 14:26:00 +0300

    Signed-off-by: fedorrb <elposhta@bigmir.net>

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace getScreenShoot
+{
+    static class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        [STAThread]
+        static void Main()
+        {
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+            Thread screenShotThread = new Thread(MakeScreenShot);
+            screenShotThread.Priority = ThreadPriority.Normal;
+            screenShotThread.IsBackground = false;
+            screenShotThread.Start();
+        }
+        private static void MakeScreenShot()
+        {
+            stPath iniFile = new stPath();
+            iniFile.LoadIniFile();
+            string path = iniFile.screenShotFolder;
+            string format = "yyyy.MM.dd HH.mm.ss";
+            string ext = iniFile.ext;
+            int delay = iniFile.delayMS;
+            while (true)
+            {
+                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+                Size screenSz = Screen.PrimaryScreen.Bounds.Size;
+                Bitmap screenshot = new Bitmap(screenSz.Width, screenSz.Height);
+                Graphics gr = Graphics.FromImage(screenshot);
+                gr.CopyFromScreen(Point.Empty, Point.Empty, screenSz);
+                string filepath = Path.Combine(path, DateTime.Now.ToString(format)) + "." + ext;
+                List<PropertyInfo> props = typeof(ImageFormat).GetProperties(BindingFlags.Static | BindingFlags.Public).ToList();
+                var imgformat = (ImageFormat)props.Find(prop => prop.Name.ToLower() == ext).GetValue(null, null);
+                screenshot.Save(filepath, imgformat);
+                Thread.Sleep(delay);
+            }
+        }
+    }
+}
---
+++ stPath.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace getScreenShoot
+{
+    public class stPath
+    {
+        public int delayMS { get; set; }//час очікування
+        public string screenShotFolder { get; set; }//папка для зберігання скріншотів
+        public string ext { get; set; } //формат зображення
+
+        public stPath()
+        {
+            delayMS = 120000;
+            screenShotFolder = "C:\\screenshots\\";
+            ext = "png";
+        }
+        /// <summary>
+        /// Чтение файла конфигурации
+        /// </summary>
+        public void LoadIniFile()
+        {
+            string path = String.Empty;
+            path = System.IO.Directory.GetCurrentDirectory();
+            path += @"\screenshot.ini";
+            int elements = 0;
+            if (System.IO.File.Exists(path))
+            {
+                try
+                {
+                    string[] allLines = System.IO.File.ReadAllLines(path);
+                    elements = allLines.Length;
+                    switch (elements)
+                    {
+                        case 3:
+                            ext = allLines[2];
+                            goto case 2;
+                        case 2:
+                            delayMS = int.Parse(allLines[1]);
+                            goto case 1;
+                        case 1:
+                            screenShotFolder = allLines[0];
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+                catch
+                {
+                    delayMS = 120000;
+                    screenShotFolder = "C:\\screenshots\\";
+                    ext = "png";
+                }
+            }
+
+            if (delayMS < 10000)
+                delayMS = 10000;
+
+            if (Directory.Exists(screenShotFolder) == false)
+            {
+                try
+                {
+                    Directory.CreateDirectory(screenShotFolder);
+                }
+                catch
+                {
+                    screenShotFolder = "C:\\screenshots\\";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Запись файла конфигурации
+        /// </summary>
+        public void SaveIniFile()
+        {
+            string[] s = { screenShotFolder,
+                             delayMS.ToString(),
+                             ext
+                         };
+            string path = String.Empty;
+            path = Application.StartupPath;
+            path += @"\screenshot.ini";
+            System.IO.File.WriteAllLines(path, s);
+        }
+    }
+}
